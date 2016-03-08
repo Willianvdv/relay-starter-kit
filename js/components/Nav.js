5981da3156a5d45e00d299f96d0cf70839db0e4b
Willian van der Velde <@willian.io>
2016-03-08 21:02:55 +0100

    branding

--- js/components/Nav.js
+++ js/components/Nav.js
@@ -11,7 +11,7 @@ class Nav extends React.Component {
       <nav className="navbar navbar-default">
         <div className="container-fluid">
           <div className="navbar-header">
-            <a className="navbar-brand" href="#">Relay</a>
+            <a className="navbar-brand" href="#">Solidus on Relay</a>
             <ul className="nav navbar-nav">
               <li>
                 <Link to={`/cart`}>
