9218b99d4148fdbc81f3d2a056d5ada92bfd0952
Willian van der Velde <@willian.io>
2016-03-12 13:01:47 +0100

    Lazy taxonomies

--- js/components/Nav/Taxonomy.js
+++ js/components/Nav/Taxonomy.js
@@ -9,7 +9,7 @@ type Props<Config> = {
     __dataID__: string,
     id: string,
     name: string,
-    taxons: [{
+    taxons?: [{
       __dataID__: string,
       id: string,
       name: string,
@@ -21,6 +21,30 @@ type Props<Config> = {
 type State<Config> = {}
 
 class Taxonomy extends React.Component<void, Props, State> {
+  constructor() {
+    super()
+    this._handleOnClick = this._handleOnClick.bind(this);
+  }
+
+  _handleOnClick() {
+    this.props.relay.setVariables({ expanded: true });
+  }
+
+  _renderTaxonomy(taxons) {
+    if (!taxons)
+      return null;
+
+    return (
+      taxons.map(taxon =>
+        <li key={taxon.id}>
+          <Link to={'/t/' + taxon.permalink}>
+            {taxon.name}
+          </Link>
+        </li>
+      )
+    )
+  }
+
   render() {
     const { taxonomy } = this.props;
 
@@ -28,20 +52,13 @@ class Taxonomy extends React.Component<void, Props, State> {
       <li className="dropdown" key={taxonomy.id}>
         <a href="#"
           className="dropdown-toggle"
+          onClick={this._handleOnClick}
           data-toggle="dropdown"
-          role="button"
-          aria-haspopup="true"
-          aria-expanded="false">
+          role="button">
             {taxonomy.name}<span className="caret"></span>
         </a>
         <ul className="dropdown-menu" role="menu" aria-labelledby="dLabel">
-          {taxonomy.taxons.map(taxon =>
-            <li key={taxon.id}>
-              <Link to={'/t/' + taxon.permalink}>
-                {taxon.name}
-              </Link>
-            </li>
-          )}
+          {this._renderTaxonomy(taxonomy.taxons)}
         </ul>
       </li>
     );
@@ -49,12 +66,22 @@ class Taxonomy extends React.Component<void, Props, State> {
 }
 
 export default Relay.createContainer(Taxonomy, {
+  initialVariables: {
+    expanded: false
+  },
+
   fragments: {
     taxonomy: () => Relay.QL`
       fragment on Taxonomy {
         id
         name
-        taxons { id name permalink }
+        ... @include(if: $expanded) {
+          taxons {
+            id
+            name
+            permalink
+          }
+        }
       }
     `,
   }
